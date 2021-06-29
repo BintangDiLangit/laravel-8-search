5c6c05396e592417b48e0ad9ca0c331b53fdcec5
bintangdilangit <syekhuda463@gmail.com>
2021-06-29 18:24:55 +0700

    first

---
+++ search-laravel/app/Http/Controllers/SearchController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Attraction;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function index(Request $request){
+        $attractions = Attraction::where('tempat_wisata', 'LIKE', '%'.$request->search.'%')
+        ->orWhere('provinsi', 'LIKE','%'.$request->search.'%')->get();
+        return view('welcome',compact('attractions'));
+    }
+}
